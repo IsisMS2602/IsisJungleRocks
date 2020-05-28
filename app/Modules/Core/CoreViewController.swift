1f811cbf643b1e6118b27c62de3b81235b034bc4
IsisMS2602 <isisms2602@gmail.com>
2020-05-28 20:29:19 -0300

    feat: Filter logs by time

--- app/Modules/Core/CoreViewController.swift
+++ app/Modules/Core/CoreViewController.swift
@@ -21,6 +21,7 @@ class CoreViewController: BaseViewController, StoryboardLoadable {
         return viewController
     }
     // MARK: Outlets
+    @IBOutlet weak var totalHoursWorked: UILabel!
     @IBOutlet weak var thisWeekButtonUI: UIButton!
     @IBOutlet weak var lastWeekButtonUI: UIButton!
     @IBOutlet weak var thisMonthButtonUI: UIButton!
@@ -31,16 +32,26 @@ class CoreViewController: BaseViewController, StoryboardLoadable {
         }
     }
     @IBAction func thisWeekButton(_ sender: Any) {
-        SessionHelper.shared.logout()
+        selectedDate = Calendar.current.date(byAdding: .day, value: -7, to: Date())
+    }
+    @IBAction func lastWeekButton(_ sender: Any) {
+        selectedDate = Calendar.current.date(byAdding: .weekOfMonth, value: -1, to: Date())
+    }
+    @IBAction func thisMonthButton(_ sender: Any) {
+        selectedDate = Calendar.current.date(byAdding: .month, value: -1, to: Date())
     }
     // MARK: Variables
+    var colorArray: [UIColor] = [.systemYellow, .systemPink, .systemBlue, .systemOrange, .systemGreen, .magenta, .systemYellow, .systemPink, .systemBlue, .systemOrange, .systemGreen, .magenta]
     var selectedKey: String?
     typealias Section = (title: String, elements: [ListOption])
     var circularView = CircularProgressView()
     var userImage: String = SessionHelper.shared.currentUser?.picture ?? "logo"
-    var worklogArray: [WorkLog] = [] {didSet {coreTableView.reloadData()}}
-    var projectArray: [Project] = [] {didSet {coreTableView.reloadData()}}
+    var worklogArray: [WorkLog] = []
+    var projectArray: [Project] = []
     var dataSource: [Section] = [] { didSet {coreTableView.reloadData()}}
+    var selectedDate: Date? {didSet {dataSource = getSections()}}
+    // TODO create color map variable
+
     // MARK: Life cycle
     override func viewDidLoad() {
         self.title = "Time Tracking"
@@ -57,10 +68,12 @@ class CoreViewController: BaseViewController, StoryboardLoadable {
         setNavigationBar(image: userImage)
         getUserTimeTrakking()
     }
+
     override func viewWillAppear(_ animated: Bool) {
         super .viewWillAppear(false)
         setNavigationBar(image: userImage)
     }
+
     private func setupTableView() {
         coreTableView.dataSource = self
         coreTableView.delegate = self
@@ -72,6 +85,7 @@ class CoreViewController: BaseViewController, StoryboardLoadable {
         coreTableView.separatorStyle = .none
         coreTableView.separatorColor = .clear
     }
+
     func setButtonsUI() {
         thisWeekButtonUI.layer.cornerRadius = 20
         thisWeekButtonUI.backgroundColor = UIColor(red: 88/255, green: 97/255, blue: 121/255, alpha: 1)
@@ -80,12 +94,17 @@ class CoreViewController: BaseViewController, StoryboardLoadable {
         thisMonthButtonUI.layer.cornerRadius = 20
         thisMonthButtonUI.backgroundColor = UIColor(red: 248/255, green: 248/255, blue: 250/255, alpha: 1)
     }
+
     func getUserTimeTrakking() {
         APIManager.GetWorkLogs().request { response in
             switch response {
             case .success(let worklog) :
                 self.getUserProjects()
                 self.worklogArray = worklog
+
+                // TODO populate color map using worklog keys - use group by func
+                // TODO randomize color for each map entry
+
             case .failure:
                 let alert = UIAlertController(title: "Unable to trek your time", message: "Check your conection status", preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "Dismiss", style: .destructive))
@@ -95,6 +114,7 @@ class CoreViewController: BaseViewController, StoryboardLoadable {
             }
         }
     }
+
     func getUserProjects() {
         APIManager.GetProjects().request { response in
             switch response {
@@ -110,16 +130,24 @@ class CoreViewController: BaseViewController, StoryboardLoadable {
             }
         }
     }
+
     func getSections() -> [Section] {
         return [
-            getProjectsSection(),
-            getOtherActivitiesSection(),
-            getLastTimeLogsSection()
+            getProjectsSection(startFrom: selectedDate),
+            getOtherActivitiesSection(startFrom: selectedDate),
+            getLastTimeLogsSection(startFrom: selectedDate)
         ]
     }
-    func getProjectsSection() -> Section {
-        let filteredArray = self.worklogArray.filter { worklog in
-            worklog.category == "Development"
+
+    func getProjectsSection(startFrom date: Date? = nil) -> Section {
+        circularView.calculateCircle(layerBaseInfoArray: getProjectsTime())
+        let filteredArray = self.worklogArray
+            .filter { worklog in
+                worklog.category == "Development"
+        }
+        .filter { worklog in
+            guard let selectedDate = self.selectedDate else { return true }
+            return worklog.createdAt >= selectedDate
         }
         let categoryMap = Dictionary(grouping: filteredArray, by: { $0.issue?.projectKey })
         let elements = categoryMap.compactMap { (key, elements) -> ListOption? in
@@ -146,9 +174,13 @@ class CoreViewController: BaseViewController, StoryboardLoadable {
             title: "Projects",
             elements: elements)
     }
-    func getOtherActivitiesSection() -> Section {
+
+    func getOtherActivitiesSection(startFrom date: Date? = nil) -> Section {
         let filteredArray = self.worklogArray.filter { worklog in
             worklog.category != "Development"
+        }.filter { worklog in
+            guard let selectedDate = self.selectedDate else { return true }
+            return worklog.createdAt >= selectedDate
         }
         let categoryMap = Dictionary(grouping: filteredArray, by: { $0.category })
         let elements = categoryMap.map { (key, elements) in
@@ -158,18 +190,58 @@ class CoreViewController: BaseViewController, StoryboardLoadable {
             title: "Other activities",
             elements: elements)
     }
-    func getLastTimeLogsSection() -> Section {
-        //          biblioteca de tempo
-        //          let filteredArray = self.worklogArray.filter { worklog in
-        //              worklog.createdAt != last hours
-        //          }
-        let categoryMap = Dictionary(grouping: worklogArray, by: { $0.issue?.key })
-        let elements = categoryMap.map { ( _, elements) in
+
+    func getLastTimeLogsSection(startFrom date: Date? = nil) -> Section {
+        let today = Date()
+        let day = Calendar.current.component(.day, from: today)
+        let filteredArrayByCreatedAt = worklogArray.filter { Calendar.current.component(.day, from: $0.createdAt) == day }
+        let timeMap = Dictionary(grouping: filteredArrayByCreatedAt, by: { $0.issue?.key })
+        let elements = timeMap.map { ( _, elements) in
             ListOption.plainCell(worklogs: elements[0])
         }
         return Section(
             title: "Last time logs",
             elements: elements)
+    }
+
+    func getProjectsTime() -> [(UIColor, Int)] {
+        let filteredArray = self.worklogArray.filter { worklog in
+            worklog.category == "Development"
+        }
+        let categoryMap = Dictionary(grouping: filteredArray, by: { $0.issue?.projectKey })
+        let elements = categoryMap.map { (_, elements) -> (UIColor, Int) in
+            let timeResult = elements.reduce(0) { (acc, element) -> Int in
+                return acc + element.timeSpent
+            }
+            let colors = colorArray.remove(at: 0)
+            colorArray.append(colors)
+            totalHoursWorked.text = "\(timeResult/3600) h"
+            return (colors, timeResult)
+        }
+        return elements
+    }
+
+    func filterByMonth() -> [WorkLog] {
+        let today = Date()
+        let thisMonth = Calendar.current.component(.month, from: today)
+        let filteredArrayByCreatedAt = worklogArray.filter { Calendar.current.component(.month, from: $0.createdAt) == thisMonth }
+        return (filteredArrayByCreatedAt)
+    }
+    func filterByLastWeek() -> [WorkLog] {
+        let today = Date()
+        let thisMonth = Calendar.current.component(.month, from: today)
+        let filteredArrayByCreatedAt = worklogArray.filter { Calendar.current.component(.month, from: $0.createdAt) == thisMonth }
+        let thisWeek = Calendar.current.component(.weekOfMonth, from: today)
+        let filteredArrayByThisWeek = filteredArrayByCreatedAt.filter { Calendar.current.component(.weekOfMonth, from: $0.createdAt) == thisWeek - 1 }
+        return (filteredArrayByThisWeek)
+    }
+    func filterByThisWeek() -> [WorkLog] {
+        let today = Date()
+        let thisMonth = Calendar.current.component(.month, from: today)
+        let filteredArrayByCreatedAt = worklogArray.filter { Calendar.current.component(.month, from: $0.createdAt) == thisMonth }
+        let thisWeek = Calendar.current.component(.weekOfMonth, from: today)
+        let filteredArrayByThisWeek = filteredArrayByCreatedAt.filter { Calendar.current.component(.weekOfMonth, from: $0.createdAt) == thisWeek - 1 }
+        return filteredArrayByThisWeek
     }
 }
 extension CoreViewController: UITableViewDataSource {
@@ -191,17 +263,19 @@ extension CoreViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let element = dataSource[indexPath.section].elements[indexPath.row]
         switch element {
+            // TODO use key value to get color from color map
         case .expandableCell(let title, _, let imgUrl, let worklogs, let isExpanded):
             let cell = tableView.dequeueReusableCell(for: indexPath) as ProjectsTableViewCell
-            cell.bind(image: imgUrl, text: title, time: "\(worklogs[0].timeSpent/3600) h", tasks: "\(worklogs.count) tasks", worklogs: worklogs, isExpanded: isExpanded)
+            cell.bind(image: imgUrl, text: title, time: "\(worklogs[0].timeSpent/3600) h", tasks: "\(worklogs.count) tasks", worklogs: worklogs, isExpanded: isExpanded, color: colorArray[indexPath.row])
             return cell
         case .plainCell(let worklog):
             let cell = tableView.dequeueReusableCell(for: indexPath) as PlainTableViewCell
-            cell.bind(key: (worklog.issue?.key) ?? "Other Activities", time: (worklog.createdAt) ?? " ")
+            cell.bind(key: (worklog.issue?.key) ?? "Other Activities", time: ("\(worklog.createdAt)") ?? " ")
             return cell
         }
     }
 }
+
 extension CoreViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let element = dataSource[indexPath.section].elements[indexPath.row]
